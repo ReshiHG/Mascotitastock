00a7abb5b98ad3f8a6ce3b2af40e9745c03a0b58
RicardoNS <jricardo.navarros@gmail.com>
2026-09-13 18:31:50 -0600

    Se modifica el login con una consulta preparada

--- controladores/loginUsuario.php
+++ controladores/loginUsuario.php
@@ -1,26 +1,50 @@
 <?php
+// try {
+//   if ($_POST) {
+//     $email = $_POST["email"];
+//     $contrasenia = $_POST["contrasenia"];
+//     // echo "email: $email <br>";
+//     // echo "contraseña: $contrasenia <br>";
+
+
+//     $conexion = new PDO('mysql:host=localhost;dbname=mascotitastock', 'mascotitastockroot', 'm4sc0t1t4s');
+//     $resultado = $conexion->query("SELECT IDUsuario FROM usuario where Contrasenia = '$contrasenia' and Email = '$email' and BitActivo = 1");
+
+//     if ($resultado->rowCount() > 0) {
+//       // echo "Usuario encontrado: ";
+//       // print_r($resultado);
+//       // header("Location:pagina_inicio.php");
+//       echo '<script>window.location.href="vistas/cuerpo/pagina_inicio.php";</script>';
+//     } else {
+//       echo '<div class="alert alert-warning" role="alert">¡Correo o contraseña erronea, favor de validar!</div>';
+//     }
+//   } else {
+//   }
+// } catch (PDOException $e) { //Mostrar error
+//   echo "Error: " . $e->getMessage();
+// }
+
+
 try {
   if ($_POST) {
     $email = $_POST["email"];
     $contrasenia = $_POST["contrasenia"];
-    // echo "email: $email <br>";
-    // echo "contraseña: $contrasenia <br>";
 
+    $conexion = new PDO('mysql:host=localhost;dbname=mascotitastock', 'mascotitastockroot', 'm4sc0t1t4s');
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $conexion = new PDO('mysql:host=localhost;dbname=macotitastock', 'root', '');
-    $resultado = $conexion->query("SELECT IDUsuario FROM usuario where Contraseña = '$contrasenia' and Email = '$email' and BitActivo = 1");
+    $stmt = $conexion->prepare("SELECT IDUsuario FROM usuario WHERE Contrasenia = :contrasenia AND Email = :email AND BitActivo = 1");
+    $stmt->bindParam(':contrasenia', $contrasenia);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
 
-    if ($resultado->rowCount() > 0) {
-      // echo "Usuario encontrado: ";
-      // print_r($resultado);
-      // header("Location:pagina_inicio.php");
+    if ($stmt->rowCount() > 0) {
       echo '<script>window.location.href="vistas/cuerpo/pagina_inicio.php";</script>';
     } else {
-      echo '<div class="alert alert-warning" role="alert">¡Correo o contraseña erronea, favor de validar!</div>';
+      echo '<div class="alert alert-warning" role="alert">¡Correo o contraseña errónea, favor de validar!</div>';
     }
-  } else {
   }
-} catch (PDOException $e) { //Mostrar error
+} catch (PDOException $e) {
   echo "Error: " . $e->getMessage();
 }
-  ?>
+?>
